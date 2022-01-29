cc67288832c13c45fdc251a0fa238c2632680780
Dean-Overton <64393357+Dean-Overton@users.noreply.github.com>
2022-01-29 11:33:48 +1100

    WIP: Form input continued.

--- src/MintForm.tsx
+++ src/MintForm.tsx
@@ -1,28 +1,35 @@
-import { Button, Container, TextField, Typography } from '@mui/material';
-import { Formik, Form } from 'formik';
+import { Button, Card, Container, FormControlLabel, Switch, TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { Formik, Form, useFormik } from 'formik';
 import * as React from 'react';
 import { useRef } from 'react';
 import PreviewImage from './PreviewImage';
 
 interface Values {
     collectionName: String;
-    iconImage: any;
     email: String;
+    iconImage: any;
 }
 interface Props {
     onSubmit: (values: Values) => void;
 }
 export const MintForm: React.FC<Props> = ({onSubmit}) => {
+    const [collectionName, setCollectionName] = React.useState("");
+    const [domain, setDomain] = React.useState("");
+    const [email, setEmail] = React.useState("");
     const fileRef = useRef<HTMLInputElement>(null);
+    const isSubmitting = React.useState(false);
     
+    const Submit = (values: Values) => {
+            // This will run when the form is submitted
+            onSubmit(values);
+            console.log(values);
+        };
     return (
         <Container maxWidth="sm">
             <Formik 
-                initialValues={{collectionName: '', iconImage: null, email: ''}} 
-                onSubmit={(values) => {
-                    onSubmit(values);
-                    console.log(values);
-                }}
+                initialValues={{collectionName: "", domain: "", email: "", iconImage: null}} 
+                onSubmit={Submit}
             >{({ values, setFieldValue }) => (
                 <Form>
                     <Typography>Collection Name</Typography>
@@ -52,9 +59,6 @@ export const MintForm: React.FC<Props> = ({onSubmit}) => {
                             }}
                         />
                     </Button>
-                    {fileRef ? (
-                        <PreviewImage>
-                    ):(<></>)}
                     <Typography>Select Backdrop</Typography>
                     <Button variant="outlined" component="label">
                         Upload Backdrop
@@ -65,6 +69,52 @@ export const MintForm: React.FC<Props> = ({onSubmit}) => {
                             type="file"
                         />
                     </Button>
+                    <Typography>Available Domains</Typography>
+                    <TextField 
+                        name='domain'
+                        value={values.domain}
+                        label="Your domain name"
+                        defaultValue="www.yourawesomenft.com"
+                    />
+                    <Card variant="outlined" style={{padding: '20px', margin: '10px'}}>
+                        <Typography>Domain Registration Contact</Typography>
+                        <TextField 
+                            name='email'
+                            value={values.email}
+                            label="Email"
+                        />
+                        <TextField 
+                            name='full-name'
+                            label="Full Name"
+                        />
+                        <TextField 
+                            name='street-address'
+                            label="Street Address"
+                        />
+                        <TextField 
+                            name='suburb'
+                            label="Suburb"
+                        />
+                        <TextField 
+                            name='state'
+                            label="State"
+                        />
+                        <TextField 
+                            name='postcode'
+                            label="Postcode"
+                        />
+                        <TextField 
+                            name='Country/Region'
+                            label="Country/Region"
+                        />
+                        <FormControlLabel control={<Switch defaultChecked />} label="WHOIS PRIVACY" />
+                    </Card>
+                    <div>
+                        <Typography style={{display:'inline-block', padding: '20px'}}>Price: 0.6 SOL</Typography>
+                        <Button variant="contained" type="submit" style={{display:'inline-block', margin: '5px', padding: '10px'}}>
+                            Buy Minting Website!
+                        </Button>
+                    </div>
                 </Form>
             )}</Formik>
         </Container>
--- src/PreviewImage.tsx
+++ src/PreviewImage.tsx
@@ -1,12 +1,16 @@
 import { read } from 'fs';
 import React, { useState } from 'react';
+import { fileURLToPath } from 'url';
 
 interface PreviewProps {
     file?: Blob;
 }
 
 const PreviewImage: React.FC<PreviewProps> = ({ file }) => {
-    const [preview, SetPreview] = useState<string | null>(null);
+    const [preview, SetPreview] = useState<string | ArrayBuffer |null>(null);
+    const [imgState, setImgState] = useState({
+        //path: fileURLToPath(file)
+    });
 
     const reader = new FileReader();
     reader.readAsDataURL(file!);
@@ -14,7 +18,7 @@ const PreviewImage: React.FC<PreviewProps> = ({ file }) => {
         SetPreview(reader.result);
     };
     return (
-        <img src={preview} alt='preview'></img>
+        <></>//<img src={imgState.path} alt='preview'></img>
     )
 }
 export default PreviewImage;
